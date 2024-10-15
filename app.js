fdc1d7b8076831a7020d5bbc97202591e9f8b6a9
Vitalie Lazu <vitalie.lazu@gmail.com>
2024-10-15 12:45:26 +0300

    Send expected error message to B2C

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
+import requestID from 'express-request-id';
 
 app = express()
 app.use(morgan('dev')) //dev, combined - apache similar logs
+app.use(requestID()) // use req.id
 
 const role_map = {
   'vitalie.lazu@gmail.com': ["org-001/admin", "org-005/user"],
   'vlazu98106@gmail.com': ["org-003/user"],
+}
+
+// https://learn.microsoft.com/en-us/azure/active-directory-b2c/restful-technical-profile#returning-validation-error-message
+function createError(requestId, userMessage) {
+  return {
+    version: "1.0.0", // rest api version
+    status: 409, // should be always 409
+    requestId,
+    userMessage,
+    // "developerMessage": "Verbose description of problem and how to fix it.",
+    // "code": "API12345",
+    // "moreInfo": "https://restapi/error/API12345/moreinfo"
+  }
 }
 
 app.get('/status', (_req, res) => { res.send({ date: new Date() }) })
@@ -19,10 +34,10 @@ app.get('/user/info', (req, res) => {
     if (roles) {
       res.send({ email, roles })
     } else {
-      res.status(404).send({ error: 'User not found.' })
+      res.status(404).send(createError(req.id, 'User not found.'))
     }
   } else {
-    res.status(422).send({ error: 'Pass user email.' })
+    res.status(422).send(createError(req.id, 'Pass user email.'))
   }
 })
 
@@ -36,13 +51,13 @@ app.get('/user/is_role_valid', (req, res) => {
       if (roles.indexOf(role) >= 0) {
         res.send({ email, role })
       } else {
-        res.status(404).send({ error: 'Role not found.' })
+        res.status(404).send(createError(req.id, 'Role not found.'))
       }
     } else {
-      res.status(404).send({ error: 'User not found.' })
+      res.status(404).send(createError(req.id, 'User not found.'))
     }
   } else {
-    res.status(422).send({ error: 'Pass user email.' })
+    res.status(422).send(createError(req.id, 'Pass user email.'))
   }
 })
 
--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
+# POC for MS B2C with rest api and role selection
+
 ```
+npm run start
+
 curl -v "localhost:3000/user/info?email=vitalie.lazu@gmail.com"
 
 curl -v "localhost:3000/user/is_role_valid?email=vitalie.lazu@gmail.com&role=org-005/user"
